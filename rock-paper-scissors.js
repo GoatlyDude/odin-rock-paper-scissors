ea1e356ccbd6a43d05fbb3cd50c694fde50f8ac6
GoatlyDude <ghostlycat2005@gmail.com>
2023-02-19 23:44:36 -0500

    Create round function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,10 @@
 //Functions
 
+function game() {
+  let playerScore = 0;
+  let cpuScore = 0;
+}
+
 function getComputerChoice()
 {
   const randomInt = GetRandomInt();
@@ -11,6 +16,44 @@ function getComputerChoice()
       return "Paper";
     case 3:
       return "Scissors"
+  }
+}
+
+function getPlayerChoice()
+{
+  const playerAnswer = window.prompt("Enter your choice", "rock");
+  const firstChar = playerAnswer.charAt(0);
+  const capFirstChar = firstChar.toUpperCase();
+  const remainingChars = playerAnswer.slice(1)
+  const lowerRemainingChars = remainingChars.toLowerCase();
+  return capFirstChar + lowerRemainingChars;
+}
+
+function playRound(playerSelection, computerSelection)
+{
+  if (playerSelection === computerSelection)
+  {
+    window.alert("You Tied!"); 
+  }
+  if (playerSelection === "Rock" && computerSelection === "Paper")
+  {
+    window.alert("You Lose! Paper beats Rock");
+    cpuScore ++;
+  }
+  if (playerSelection === "Paper" && computerSelection === "Scissors")
+  {
+    window.alert("You Lose! Scissors beats Paper");
+    cpuScore ++;
+  }
+  if (playerSelection === "Scissors" && computerSelection === "Rock")
+  {
+    window.alert("You Lose! Rock beats Scissors");
+    cpuScore ++;
+  }
+  else
+  {
+    window.alert("You Win!");
+    playerScore++;
   }
 }
 
