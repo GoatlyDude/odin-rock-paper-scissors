3ad4c71bdb8a6d6fa7911beb2cde7763942a98bc
GoatlyDude <ghostlycat2005@gmail.com>
2023-02-19 23:08:42 -0500

    Finish computer choice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,8 +2,18 @@
 
 function getComputerChoice()
 {
-  
+  const randomInt = GetRandomInt();
+  switch (randomInt)
+  {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors"
+  }
 }
+
 
 function GetRandomInt()
 {
